6269d96b1a1951ae477afb0c3d5bbe133d3cd200
elias schwegler <elias.schwegler03@gmail.com>
2024-04-25 17:14:29 +0200

    Done with HTML of Spesific Product

--- src/app/product-spesific/product-spesific.component.ts
+++ src/app/product-spesific/product-spesific.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-product-spesific',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, MatDivider],
   templateUrl: './product-spesific.component.html',
   styleUrl: './product-spesific.component.scss',
 })
-export class ProductSpesificComponent {}
+export class ProductSpesificComponent {
+  title = 'TITLE';
+  description = 'DESCRIPTION';
+  PRICE = '20';
+  Features = 'Well it seems nice. Right ?';
+  Creator = 'Will Smith';
+}
