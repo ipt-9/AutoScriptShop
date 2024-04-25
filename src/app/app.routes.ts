0903523281c135257879663b8ea02b19bdd60183
elias schwegler <elias.schwegler03@gmail.com>
2024-04-25 15:56:34 +0200

    Changes by collyn

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { TESTComponent } from './test/test.component';
+import { DumpComponent } from './dump/dump.component';
+import { ProductSpesificComponent } from './product-spesific/product-spesific.component';
+import { ProductBoxComponent } from './product-box/product-box.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'products',
+    component: ProductsComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'test',
+    component: TESTComponent,
+  },
+  {
+    path: 'dump',
+    component: DumpComponent,
+  },
+  {
+    path: 'product-spesific',
+    component: ProductSpesificComponent,
+  },
+  {
+    path: 'product-box',
+    component: ProductBoxComponent,
+  },
+];
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {MatDivider} from "@angular/material/divider";
 import {MatToolbar} from "@angular/material/toolbar";
 import {NgOptimizedImage} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatDivider, MatToolbar, NgOptimizedImage],
+  imports: [MatDivider, MatToolbar, NgOptimizedImage, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
