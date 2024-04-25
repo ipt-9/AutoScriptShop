695d5ac6dccaa1f26e23f851525af8e86ac7dd2a
elias schwegler <elias.schwegler03@gmail.com>
2024-04-25 16:19:20 +0200

    Rout Problems, But it works

--- src/app/product-spesific/product-spesific.component.ts
+++ src/app/product-spesific/product-spesific.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-import {NavbarComponent} from "../navbar/navbar.component";
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-product-spesific',
   standalone: true,
-  imports: [
-    NavbarComponent
-  ],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './product-spesific.component.html',
-  styleUrl: './product-spesific.component.scss'
+  styleUrl: './product-spesific.component.scss',
 })
-export class ProductSpesificComponent {
-
-}
+export class ProductSpesificComponent {}
