40c35930d2f62ccfdf77d75c9131133e5f1c7534
Lux005 <106537362+Lux005@users.noreply.github.com>
2024-05-02 08:18:53 +0200

    Update product-box.component.ts

--- src/app/product-box/product-box.component.ts
+++ src/app/product-box/product-box.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule} from "@angular/common";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 
 @Component({
   selector: 'app-product-box',
-  standalone: true,
-  imports: [],
   templateUrl: './product-box.component.html',
-  styleUrl: './product-box.component.scss'
+  standalone: true,
+  styleUrls: ['./product-box.component.scss']
 })
-export class ProductBoxComponent {
+export class ProductBoxComponent implements OnInit {
+  httpsClient = inject(HttpClient);
+  data: any[] = [];
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  private fetchData(): void {
+    this.httpClient
+      .get('backend.auto-script-shop-bmsd21a.bbzwinf.ch/api/products')
+      .subscribe((data: any) => {
+        console.log(data);
+        this.data = data;
+      });
+  }
+  constructor(private httpClient: HttpClient) {}
+
 
 }
