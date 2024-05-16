3744f8c98e38b312a143ab42bc533d0e34b53f83
clegast <clegast512@gmail.com>
2024-05-16 15:25:23 +0200

    add offer Product logic

--- src/app/offerproduct/offerproduct.component.ts
+++ src/app/offerproduct/offerproduct.component.ts
@@ -3,6 +3,8 @@ import { MatCheckbox } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-offerproduct',
@@ -12,8 +14,38 @@ import { FooterComponent } from '../footer/footer.component';
   styleUrl: './offerproduct.component.scss',
 })
 export class OfferproductComponent {
-  checked: 'false' | undefined;
+  checked:boolean= false;
+  title?: string;
+  body?: string;
+  features?: string;
+  price?:number;
+  script?: string;
+  token?:any;
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) {}
+
   submitForm() {
-    // only submitable if AGB accepted!
+    if (!this.checked || !this.title || !this.body || !this.features || !this.price || !this.script || !sessionStorage.getItem('token')) {
+      return
+    }
+
+    const token = sessionStorage.getItem('token');
+    this.token = token
+
+    const body = {
+      title: this.title,
+      body: this.body,
+      features: this.features,
+      price: this.price,
+      script: this.script
+    };
+    const headers = new HttpHeaders({'Authorization': `Bearer ${{token}}`});
+    this.http.post<any>('http://127.0.0.1:8000/api/products/create/', body, {headers: headers}).subscribe({
+      next: () => console.log("success"),
+      error: (err) => console.error('error: ', err)
+    })
   }
 }
