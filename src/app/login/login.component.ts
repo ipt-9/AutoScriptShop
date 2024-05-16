16c4b4cac3615a88a192e4d443d751d87888149f
elias schwegler <elias.schwegler03@gmail.com>
2024-05-16 09:59:34 +0200

    Design of Sign-UP started

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,18 +2,27 @@ import { Component } from '@angular/core';
 import {NavbarComponent} from "../navbar/navbar.component";
 import {FooterComponent} from "../footer/footer.component";
 import {RouterLink} from "@angular/router";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [
-    NavbarComponent,
-    FooterComponent,
-    RouterLink
-  ],
+    imports: [
+        NavbarComponent,
+        FooterComponent,
+        RouterLink,
+        MatFormField,
+        MatInput,
+        MatLabel
+    ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  email?:string;
+  onKeyEmail(value:string) {
+    this.email = value
+  }
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {NavbarComponent} from "../navbar/navbar.component";
+import {FooterComponent} from "../footer/footer.component";
 import {HttpClient} from "@angular/common/http";
 import {MatFormField} from "@angular/material/form-field";
 import {MatInputModule} from '@angular/material/input'
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {TextFieldModule} from '@angular/cdk/text-field';
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
   imports: [
     NavbarComponent,
-    MatFormField
+    MatFormFieldModule,
+    MatInputModule,
+    TextFieldModule,
+    MatButtonModule,
+    FooterComponent,
+    MatIcon
   ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
@@ -23,13 +33,15 @@ export class SignUpComponent implements OnInit{
 
   postId:any;
 
-
   constructor(
     private http: HttpClient
   ) {}
 
   ngOnInit() {
     //const headers = { 'Authorization': 'Bearer my-token', 'My-Custom-Header': 'foobar' };
+  }
+
+  createUser() {
     const body = {
       username: this.Username,
       email: this.email,
@@ -38,8 +50,24 @@ export class SignUpComponent implements OnInit{
       password: this.password,
     };
 
-    this.http.post<any>('https://backend.auto-script-shop-bmsd21a.bbzwinf.ch/api/products/id/', body).subscribe(data => {
+    this.http.post<any>('https://backend.auto-script-shop-bmsd21a.bbzwinf.ch/api/user/create/', body).subscribe(data => {
       this.postId = data.id;
     });
+  }
+
+  onKeyUsername(value:string) {
+    this.Username = value
+  }
+  onKeyEmail(value:string) {
+    this.email = value
+  }
+  onKeyFirstname(value:string) {
+    this.firstName = value
+  }
+  onKeyLastname(value:string) {
+    this.lastName = value
+  }
+  onKeyPassword(value:string) {
+    this.password = value
   }
 }
