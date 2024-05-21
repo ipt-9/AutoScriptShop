fc6e572448bce7a3e82592a361296f51d74747c4
clegast <clegast512@gmail.com>
2024-05-21 21:01:43 +0200

    remove unnecessary files and Directories

--- src/app/offerproduct/offerproduct.component.ts
+++ src/app/offerproduct/offerproduct.component.ts
@@ -26,7 +26,6 @@ export class OfferproductComponent {
   features?: string;
   price?: number;
   script?: string;
-  token?: any;
 
   constructor(
     private http: HttpClient,
@@ -47,7 +46,6 @@ export class OfferproductComponent {
     }
 
     const token = sessionStorage.getItem('token');
-    this.token = token;
 
     const body = {
       title: this.title,
@@ -56,10 +54,10 @@ export class OfferproductComponent {
       price: this.price,
       script: this.script,
     };
-    const headers = new HttpHeaders({ Authorization: `Bearer ${{ token }}` });
+    const headers = new HttpHeaders({ Authorization: `Bearer ${ token }` });
     this.http
       .post<any>(
-        'https://backend.auto-script-shop-bmsd21a.bbzwinf.ch/api/products/create/',
+        'http://127.0.0.1:8000/api/products/create/',
         body,
         { headers: headers },
       )
